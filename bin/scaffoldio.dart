9f004bf776a8eb94416fcb6c4d7a39374105d364
HirenMakwana45 <hirenm563@gmail.com>
2026-02-25 13:43:23 +0530

    updated pubspec.yaml & scaffoldio.dart file

--- bin/scaffoldio.dart
+++ bin/scaffoldio.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:args/args.dart';
 
-const String version = '0.0.6';
+const String version = '0.0.8';
 
 void main(List<String> arguments) async {
   final ArgParser argParser = ArgParser()
